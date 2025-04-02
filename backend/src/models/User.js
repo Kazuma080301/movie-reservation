ec5894682be3014f16a3ec006f524ca9b7cd3094
Manik Razdan <kazuma@Maniks-MacBook-Air.local>
2025-04-03 03:44:36 +0530

    test commit

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -11,4 +11,11 @@ const userSchema = new mongoose.Schema({
     }
 }, { timestamps: true });
 
+// Hash password before saving
+// userSchema.pre("save", async function(next) {
+//     if (!this.isModified("password")) return next();
+//     this.password = await bcrypt.hash(this.password, 10);
+//     next();
+// });
+
 module.exports = mongoose.model('User', userSchema);
