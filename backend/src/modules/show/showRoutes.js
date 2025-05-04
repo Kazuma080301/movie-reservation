db92a5762f6775c5e33424e6a6f0b7ea2c36e87f
Manik Razdan <kazuma@Maniks-MacBook-Air.local>
2025-05-04 22:29:23 +0530

    extra get seats route added

--- backend/src/modules/show/showRoutes.js
+++ backend/src/modules/show/showRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const showSchema = require('./showValidator');
 const { validateSchema } = require('../../middlewares/validateMiddleware')
-const { getShows } = require('./showController')
+const { getShows, getSeatsForShow } = require('./showController')
 
 const router = express.Router();
 
-router.get('/shows', validateSchema(showSchema), getShows);
+router.get('/', validateSchema(showSchema), getShows);
 
-
+router.get('/seats', getSeatsForShow)
 
 module.exports = router;
